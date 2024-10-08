a1d55a6e57ff1255b91570cdfe9157d53e508709
KOUSHIK PINNU <pinnukoushik1@gmail.com>
2024-10-08 15:58:07 +0530

    Added file 1

--- a.bat
+++ a.bat
@@ -1,39 +1,23 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Set the file to modify
-set inputFile=c.txt
-set tempFile=temp.txt
+:: Create the temp folder if it doesn't exist
+if not exist temp (
+    mkdir temp
+)
 
 :: Loop 100 times
-for /l %%i in (1,1,100) do (
+for /l %%i in (1,1,50) do (
     echo Loop iteration %%i
 
-    :: Step 1: Run git add, commit, and push
+    :: Step 1: Create a new file in the temp folder and write some text to it
+    set fileName=temp\file%%i.txt
+    echo This is file %%i > !fileName!
+    echo Created file !fileName! with content: "This is file %%i"
+
+    :: Step 2: Run git add, commit, and push
     git add *
-    git commit -m "."
-    git push origin main
-
-    :: Step 2: Modify the file by removing "hello world" and replacing it with "hello"
-    if not exist "%inputFile%" (
-        echo File not found!
-        exit /b
-    )
-
-    > "%tempFile%" (
-        for /f "tokens=*" %%A in (%inputFile%) do (
-            set line=%%A
-            set line=!line:hello world=hello!
-            echo !line!
-        )
-    )
-
-    move /y "%tempFile%" "%inputFile%"
-    echo "hello world" has been replaced with "hello"
-
-    :: Step 3: Add, commit, and push the changes after modification
-    git add *
-    git commit -m "Reverted 'hello world' to 'hello'"
+    git commit -m "Added file %%i"
     git push origin main
 
     echo Iteration %%i completed.
