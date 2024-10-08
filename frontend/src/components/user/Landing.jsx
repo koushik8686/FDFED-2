11efda0f5fb31e6d25843253da737d0069e6eefe
KOUSHIK PINNU <pinnukoushik1@gmail.com>
2024-10-08 15:19:31 +0530

    .

--- frontend/src/components/user/Landing.jsx
+++ frontend/src/components/user/Landing.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 import './Landing.css';  // Import the CSS file
 import { useEffect , useState} from "react";
 
-export default function Component() {
+//landing page
 
-  
+export default function Component() {
   return (
     <div  className="page-container">
       <header className="header">
